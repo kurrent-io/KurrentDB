5e4743f7cff494c1d1f513cb6647f3ac5c657df7
William Chong <william-chong@outlook.com>
2024-06-27 19:13:44 +0400

    RenameConnector command tests

---
+++ src/EventStore.Plugins.Connectors/tests/EventStore.Extensions.Connectors.Tests/Planes/Management/ConnectorApplication/RenameConnectorCommandTests.cs
@@ -0,0 +1,69 @@
+using EventStore.Connectors.Management;
+using EventStore.Connectors.Management.Contracts.Commands;
+using EventStore.Connectors.Management.Contracts.Events;
+using EventStore.Extensions.Connectors.Tests.CommandService;
+using EventStore.Testing.Fixtures;
+using Google.Protobuf.WellKnownTypes;
+
+namespace EventStore.Extensions.Connectors.Tests.Management.ConnectorApplication;
+
+public class RenameConnectorCommandTests(ITestOutputHelper output, CommandServiceFixture fixture)
+    : FastTests<CommandServiceFixture>(output, fixture) {
+    [Fact]
+    public async Task rename_connector_when_connector_exists() {
+        var connectorId      = Fixture.NewConnectorId();
+        var connectorName    = Fixture.NewConnectorName();
+        var newConnectorName = Fixture.NewConnectorName();
+
+        await CommandServiceSpec<ConnectorEntity, RenameConnector>.Builder
+            .WithService(Fixture.CreateConnectorApplication)
+            .Given(
+                new ConnectorCreated {
+                    ConnectorId = connectorId,
+                    Name        = connectorName,
+                    Timestamp   = Fixture.TimeProvider.GetUtcNow().ToTimestamp()
+                }
+            )
+            .When(
+                new RenameConnector {
+                    ConnectorId = connectorId,
+                    Name        = newConnectorName
+                }
+            )
+            .Then(
+                new ConnectorRenamed {
+                    ConnectorId = connectorId,
+                    Name        = newConnectorName,
+                    Timestamp   = Fixture.TimeProvider.GetUtcNow().ToTimestamp()
+                }
+            );
+    }
+
+    [Fact]
+    public async Task connector_deleted_exception_when_renaming_deleted_connector() {
+        var connectorId      = Fixture.NewConnectorId();
+        var connectorName    = Fixture.NewConnectorName();
+        var newConnectorName = Fixture.NewConnectorName();
+
+        await CommandServiceSpec<ConnectorEntity, RenameConnector>.Builder
+            .WithService(Fixture.CreateConnectorApplication)
+            .Given(
+                new ConnectorCreated {
+                    ConnectorId = connectorId,
+                    Name        = connectorName,
+                    Timestamp   = Fixture.TimeProvider.GetUtcNow().ToTimestamp()
+                },
+                new ConnectorDeleted {
+                    ConnectorId = connectorId,
+                    Timestamp   = Fixture.TimeProvider.GetUtcNow().ToTimestamp()
+                }
+            )
+            .When(
+                new RenameConnector {
+                    ConnectorId = connectorId,
+                    Name        = newConnectorName
+                }
+            )
+            .Then(new ConnectorDomainExceptions.ConnectorDeleted(connectorId));
+    }
+}
