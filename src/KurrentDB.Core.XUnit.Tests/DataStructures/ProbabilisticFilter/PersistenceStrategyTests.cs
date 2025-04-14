0aa4b018acd498874352a1f1317a8d283e867802
Timothy Coleman <timothy.coleman@gmail.com>
2025-04-14 15:44:42 +0100

    Use directory per tests rather than shared directory fixture
    I think this must have worked before by coincidence of test ordering

--- src/KurrentDB.Core.XUnit.Tests/DataStructures/ProbabilisticFilter/PersistenceStrategyTests.cs
+++ src/KurrentDB.Core.XUnit.Tests/DataStructures/ProbabilisticFilter/PersistenceStrategyTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace KurrentDB.Core.XUnit.Tests.DataStructures.ProbabilisticFilter;
 
-public abstract class PersistenceStrategyTests {
+public abstract class PersistenceStrategyTests : DirectoryPerTest<PersistenceStrategyTests> {
 	protected abstract IPersistenceStrategy GenSut(
 		long size, bool create, string fileName);
 
@@ -27,7 +27,7 @@ public abstract class PersistenceStrategyTests {
 
 	[Fact]
 	public void AfterInitializationIsFilledWithZeroes() {
-		using (var sut = CreateSut(10_000, "filledWithZeroesFilter")) {
+		using (var sut = CreateSut(10_000)) {
 			for (var bitPosition = 0; bitPosition < 10_000 * 8; bitPosition++) {
 				Assert.False(sut.DataAccessor.IsBitSet(bitPosition));
 			}
@@ -36,7 +36,7 @@ public abstract class PersistenceStrategyTests {
 		}
 
 		// and after reopening
-		using (var sut = OpenSut(10_000, "filledWithZeroesFilter")) {
+		using (var sut = OpenSut(10_000)) {
 			for (var bitPosition = 0; bitPosition < 10_000 * 8; bitPosition++) {
 				Assert.False(sut.DataAccessor.IsBitSet(bitPosition));
 			}
@@ -62,43 +62,21 @@ public abstract class PersistenceStrategyTests {
 		}
 	}
 
-	public class MemoryMappedFilePersistenceTests :
-		PersistenceStrategyTests,
-		IClassFixture<DirectoryFixture<MemoryMappedFilePersistenceTests>> {
-
-		private readonly DirectoryFixture<MemoryMappedFilePersistenceTests> _fixture;
-
-		public MemoryMappedFilePersistenceTests(
-			DirectoryFixture<MemoryMappedFilePersistenceTests> fixture) {
-
-			_fixture = fixture;
-		}
-
+	public class MemoryMappedFilePersistenceTests : PersistenceStrategyTests {
 		protected override IPersistenceStrategy GenSut(
 			long size, bool create, string fileName) {
 
 			return new MemoryMappedFilePersistence(
-				size, _fixture.GetFilePathFor(fileName), create);
+				size, Fixture.GetFilePathFor(fileName), create);
 		}
 	}
 
-	public class FileStreamFilePersistenceTests :
-		PersistenceStrategyTests,
-		IClassFixture<DirectoryFixture<FileStreamFilePersistenceTests>> {
-
-		private readonly DirectoryFixture<FileStreamFilePersistenceTests> _fixture;
-
-		public FileStreamFilePersistenceTests(
-			DirectoryFixture<FileStreamFilePersistenceTests> fixture) {
-
-			_fixture = fixture;
-		}
-
+	public class FileStreamFilePersistenceTests : PersistenceStrategyTests {
 		protected override FileStreamPersistence GenSut(
 			long size, bool create, string fileName) {
 
 			return new FileStreamPersistence(
-				size, _fixture.GetFilePathFor(fileName), create);
+				size, Fixture.GetFilePathFor(fileName), create);
 		}
 
 		[Theory]
