kurrent-io/EventStore

DEVEX-279: Add schema information to events and log records
Introduced `SchemaInfo` to support schema format and schema versioning for events. Updated constructors, methods, and classes to include `dataSchemaInfo` and `metadataSchemaInfo` parameters. Ensured backward compatibility by defaulting schema info to `None` where not explicitly provided.
There is still much to be done.

src/EventStore.Core/TransactionLog/LogRecords/LogV3StreamWriteRecord.cs
@@ -3,6 +3,7 @@
 
 using System;
 using EventStore.Common.Utils;
+using EventStore.Core.Data;
 using EventStore.Core.LogV3;
 using EventStore.LogCommon;
 using EventStore.LogV3;
@@ -30,7 +31,9 @@ public class LogV3StreamWriteRecord : LogV3Record<StreamWriteRecord>, IEquatable
 		PrepareFlags flags,
 		uint eventType,
 		ReadOnlySpan<byte> data,
-		ReadOnlySpan<byte> metadata) {
+		ReadOnlySpan<byte> metadata,
+        SchemaInfo dataSchemaInfo,
+        SchemaInfo metadataSchemaInfo) {
 
 		Ensure.Nonnegative(logPosition, "logPosition");
 		Ensure.NotEmptyGuid(correlationId, "correlationId");
@@ -86,7 +89,9 @@ public class LogV3StreamWriteRecord : LogV3Record<StreamWriteRecord>, IEquatable
 			flags: Flags,
 			eventType: EventType,
 			data: Data.Span,
-			metadata: Metadata.Span);
+			metadata: Metadata.Span,
+            dataSchemaInfo: SchemaInfo.None,
+            metadataSchemaInfo: SchemaInfo.None);
 	}
 
 	public bool Equals(LogV3StreamWriteRecord other) {

kurrent-io/KurrentDB

src/KurrentDB.Core/TransactionLog/LogRecords/LogV3StreamWriteRecord.cs
@@ -2,6 +2,8 @@
 // Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
 using System;
+using KurrentDB.Common.Utils;
+using KurrentDB.Core.Data;
 using EventStore.Core.LogV3;
 using KurrentDB.Common.Utils;
 using KurrentDB.LogCommon;
@@ -30,7 +32,9 @@ public class LogV3StreamWriteRecord : LogV3Record<StreamWriteRecord>, IEquatable
 		PrepareFlags flags,
 		uint eventType,
 		ReadOnlySpan<byte> data,
-		ReadOnlySpan<byte> metadata) {
+		ReadOnlySpan<byte> metadata,
+        SchemaInfo dataSchemaInfo,
+        SchemaInfo metadataSchemaInfo) {
 
 		Ensure.Nonnegative(logPosition, "logPosition");
 		Ensure.NotEmptyGuid(correlationId, "correlationId");
@@ -86,7 +90,9 @@ public class LogV3StreamWriteRecord : LogV3Record<StreamWriteRecord>, IEquatable
 			flags: Flags,
 			eventType: EventType,
 			data: Data.Span,
-			metadata: Metadata.Span);
+			metadata: Metadata.Span,
+            dataSchemaInfo: SchemaInfo.None,
+            metadataSchemaInfo: SchemaInfo.None);
 	}
 
 	public bool Equals(LogV3StreamWriteRecord other) {