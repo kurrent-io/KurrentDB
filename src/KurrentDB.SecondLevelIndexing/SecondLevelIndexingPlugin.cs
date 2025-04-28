c61c8d878fe9d809a92255bad5e4b842144eb10c
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-04-28 13:06:18 +0200

    Added Second Level Indexing Plugin and DummyVirtualStreamReader to verify pluggability

---
+++ src/KurrentDB.SecondLevelIndexing/SecondLevelIndexingPlugin.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
+// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
+
+using EventStore.Plugins;
+using KurrentDB.Core.Configuration.Sources;
+using KurrentDB.POC.IO.Core;
+using Microsoft.Extensions.Configuration;
+
+namespace KurrentDB.SecondLevelIndexing;
+
+public class SecondLevelIndexingPlugin() : SubsystemsPlugin(name: "second-level-indexing"), IConnectedSubsystemsPlugin {
+	public override (bool Enabled, string EnableInstructions) IsEnabled(IConfiguration configuration) {
+		var enabledOption =
+			configuration.GetValue<bool?>($"{KurrentConfigurationKeys.Prefix}:SecondLevelIndexing:Enabled");
+		var devMode = configuration.GetValue($"{KurrentConfigurationKeys.Prefix}:Dev", defaultValue: false);
+
+		// Enabled by default only in the dev mode
+		// TODO: Change it to be enabled by default when work on 2nd-level indexing is finished
+		bool enabled = enabledOption ?? devMode;
+
+		return enabled
+			? (true, "")
+			: (false,
+				$"To enable Second Level Indexing Set '{KurrentConfigurationKeys.Prefix}:SecondLevelIndexing:Enabled' to 'true'");
+	}
+}
