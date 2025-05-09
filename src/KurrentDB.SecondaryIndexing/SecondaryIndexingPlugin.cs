2be010e0a71f4d42443d817d9086c64ab03f8ff2
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-05-09 15:30:08 +0200

    Exposed ReadIndex in ClusterVNode to enable configuring secondary indexing

--- src/KurrentDB.SecondaryIndexing/SecondaryIndexingPlugin.cs
+++ src/KurrentDB.SecondaryIndexing/SecondaryIndexingPlugin.cs
@@ -6,10 +6,12 @@ using EventStore.Plugins;
 using EventStore.Plugins.Subsystems;
 using KurrentDB.Core.Bus;
 using KurrentDB.Core.Configuration.Sources;
+using KurrentDB.Core.Messages;
 using KurrentDB.Core.Services.Storage.InMemory;
 using KurrentDB.Core.Services.Storage.ReaderIndex;
 using KurrentDB.SecondaryIndexing.Builders;
 using KurrentDB.SecondaryIndexing.Indices;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
 namespace KurrentDB.SecondaryIndexing;
@@ -17,7 +19,7 @@ namespace KurrentDB.SecondaryIndexing;
 public class SecondaryIndexPluginConfigurationOptions<TStreamId> {
 	public required IPublisher Publisher { get; set; }
 	public required ISubscriber Subscriber { get; set; }
-	public required IReadIndex<TStreamId> Index { get; set; }
+	public required IReadIndex<TStreamId> ReadIndex { get; set; }
 }
 
 public interface ISecondaryIndexingPlugin : ISubsystemsPlugin {
@@ -31,16 +33,26 @@ public static class SecondaryIndexingPluginFactory {
 		new SecondaryIndexingPlugin();
 }
 
-internal class SecondaryIndexingPlugin(ISecondaryIndex? index = null) : SubsystemsPlugin(name: "secondary-indexing"), ISecondaryIndexingPlugin {
-	private SecondaryIndexBuilder _secondaryIndexBuilder = null!;
+internal class SecondaryIndexingPlugin(ISecondaryIndex? index = null)
+	: SubsystemsPlugin(name: "secondary-indexing"), ISecondaryIndexingPlugin {
+	private SecondaryIndexBuilder? _secondaryIndexBuilder;
 
 	public IEnumerable<IVirtualStreamReader> IndicesVirtualStreamReaders =>
-		_secondaryIndexBuilder.IndexVirtualStreamReaders;
+		_secondaryIndexBuilder?.IndexVirtualStreamReaders ?? [];
+
+	public override void ConfigureApplication(IApplicationBuilder app, IConfiguration configuration) {
+
+		base.ConfigureApplication(app, configuration);
+	}
 
 	[Experimental("SECONDARYINDEXING")]
 	public void Configure<TStreamId>(SecondaryIndexPluginConfigurationOptions<TStreamId> configurationOptions) {
-		if(index != null)
-			_secondaryIndexBuilder = new SecondaryIndexBuilder(index, configurationOptions.Publisher);
+		if (index == null) return;
+
+		_secondaryIndexBuilder = new SecondaryIndexBuilder(index, configurationOptions.Publisher);
+
+		configurationOptions.Subscriber.Subscribe<SystemMessage.SystemReady>(_secondaryIndexBuilder);
+		configurationOptions.Subscriber.Subscribe<SystemMessage.BecomeShuttingDown>(_secondaryIndexBuilder);
 	}
 
 	public override (bool Enabled, string EnableInstructions) IsEnabled(IConfiguration configuration) {
