b6af00fb44e3eb415ade130f5bf3b7e3934f64ed
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-05-09 13:14:42 +0200

    Moved Secondary Index Checkpointing to a dedicated SecondaryIndexCheckpointTracker
    Using Subject and putting there bool data worked, but it was kind artificial, as what we actually need is timer and counter to know whether to commit or not. Implemented that as part of SecondaryIndexCheckpointTracker.

---
+++ src/KurrentDB.SecondaryIndexing.Tests/Subscriptions/SecondaryIndexCheckpointTrackerTests.cs
@@ -0,0 +1,297 @@
+// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
+// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
+
+using KurrentDB.SecondaryIndexing.Subscriptions;
+
+namespace KurrentDB.SecondaryIndexing.Tests.Subscriptions;
+
+public class SecondaryIndexCheckpointTrackerTests {
+	[Fact]
+	public async Task Commits_On_Threshold() {
+		// Given
+		var callCount = 0;
+		var commitSignal = new ManualResetEventSlim(false);
+		var batchSize = 0;
+
+		var tracker = new SecondaryIndexCheckpointTracker(5, 10000, _ => {
+			Interlocked.Increment(ref callCount);
+			batchSize = 5;
+			commitSignal.Set();
+			return Task.CompletedTask;
+		});
+
+		tracker.Start(CancellationToken.None);
+
+		// When
+		for (int i = 0; i < 5; i++) {
+			tracker.Increment();
+		}
+
+		var committed = commitSignal.Wait(TimeSpan.FromMilliseconds(100));
+		await tracker.DisposeAsync();
+
+		// Then
+		Assert.True(committed);
+		Assert.Equal(1, callCount);
+		Assert.Equal(5, batchSize);
+	}
+
+	[Fact]
+	public async Task Commits_On_Timer() {
+		// Given
+		var callCount = 0;
+		var commitSignal = new ManualResetEventSlim(false);
+
+		var tracker = new SecondaryIndexCheckpointTracker(1000, 10, _ => {
+			Interlocked.Increment(ref callCount);
+			commitSignal.Set();
+			return Task.CompletedTask;
+		});
+
+		tracker.Start(CancellationToken.None);
+
+		// When
+		tracker.Increment();
+		var committed = commitSignal.Wait(TimeSpan.FromMilliseconds(100));
+		await tracker.DisposeAsync();
+
+		// Then
+		Assert.True(committed);
+		Assert.Equal(1, callCount);
+	}
+
+	[Fact]
+	public async Task Commit_Is_Not_Reentrant() {
+		// Given
+		var commitInProgress = new ManualResetEventSlim(false);
+		var commitReleaser = new ManualResetEventSlim(false);
+		var maxConcurrentCommits = 0;
+		var currentConcurrentCommits = 0;
+
+		var tracker = new SecondaryIndexCheckpointTracker(3, 10000, async ct => {
+			var currentCommits = Interlocked.Increment(ref currentConcurrentCommits);
+			maxConcurrentCommits = Math.Max(maxConcurrentCommits, currentCommits);
+
+			commitInProgress.Set();
+
+			commitReleaser.Wait();
+
+			Interlocked.Decrement(ref currentConcurrentCommits);
+			await Task.CompletedTask;
+		});
+
+		tracker.Start(CancellationToken.None);
+
+		// When
+		for (int i = 0; i < 3; i++) tracker.Increment();
+
+		Assert.True(commitInProgress.Wait(100));
+
+		for (int i = 0; i < 3; i++) tracker.Increment();
+
+		commitReleaser.Set();
+
+		// Then
+		await tracker.DisposeAsync();
+
+		Assert.Equal(1, maxConcurrentCommits);
+	}
+
+	[Fact]
+	public async Task Multiple_Threads_Cause_No_Double_Commits() {
+		// Given
+		var callCount = 0;
+		var commitSignal = new ManualResetEventSlim(false);
+
+		var tracker = new SecondaryIndexCheckpointTracker(100, 5000, _ => {
+			Interlocked.Increment(ref callCount);
+			commitSignal.Set();
+			return Task.CompletedTask;
+		});
+
+		tracker.Start(CancellationToken.None);
+
+		// When
+		var tasks = Enumerable.Range(0, 100)
+			.Select(_ => Task.Run(() => tracker.Increment()))
+			.ToArray();
+
+		await Task.WhenAll(tasks);
+
+		var committed = commitSignal.Wait(TimeSpan.FromMilliseconds(100));
+		await tracker.DisposeAsync();
+
+		// Then
+		Assert.True(committed);
+		Assert.Equal(1, callCount);
+	}
+
+	[Fact]
+	public async Task Does_Not_Commit_Empty() {
+		// Given
+		var callCount = 0;
+
+		var tracker = new SecondaryIndexCheckpointTracker(1000, 10, _ => {
+			Interlocked.Increment(ref callCount);
+			return Task.CompletedTask;
+		});
+
+		// When
+		tracker.Start(CancellationToken.None);
+		await Task.Delay(30);
+		await tracker.DisposeAsync();
+
+		// Then
+		Assert.Equal(0, callCount);
+	}
+
+	[Fact]
+	public async Task Respects_Cancellation() {
+		// Given
+		var callCount = 0;
+		var cts = new CancellationTokenSource();
+
+		var tracker = new SecondaryIndexCheckpointTracker(1, 1000, _ => {
+			Interlocked.Increment(ref callCount);
+			return Task.CompletedTask;
+		});
+
+		// When
+		tracker.Start(cts.Token);
+		await cts.CancelAsync();
+		tracker.Increment();
+
+		await Task.Delay(30);
+		await tracker.DisposeAsync();
+
+		// Then
+		Assert.Equal(0, callCount);
+	}
+
+	[Fact]
+	public async Task Concurrent_Start_Calls_Throw_Exception() {
+		// Given
+		var tracker = new SecondaryIndexCheckpointTracker(10, 100, _ => Task.CompletedTask);
+
+		// When
+		tracker.Start(CancellationToken.None);
+
+		// Then
+		var ex = Assert.Throws<InvalidOperationException>(() =>
+			tracker.Start(CancellationToken.None));
+
+		Assert.Contains("Already started", ex.Message);
+
+		await tracker.DisposeAsync();
+	}
+
+	[Fact]
+	public async Task Increment_After_Dispose_Throws() {
+		// Given
+		var tracker = new SecondaryIndexCheckpointTracker(10, 100, _ => Task.CompletedTask);
+
+		// When
+		tracker.Start(CancellationToken.None);
+		await tracker.DisposeAsync();
+
+		// Then
+		var ex = Assert.Throws<ObjectDisposedException>(() => tracker.Increment());
+		Assert.Contains("SecondaryIndexCheckpointTracker", ex.Message);
+	}
+
+	[Fact]
+	public async Task Start_After_Dispose_Throws() {
+		// Given
+		var tracker = new SecondaryIndexCheckpointTracker(10, 100, _ => Task.CompletedTask);
+
+		// When
+		await tracker.DisposeAsync();
+
+		// Then
+		var ex = Assert.Throws<ObjectDisposedException>(() =>
+			tracker.Start(CancellationToken.None));
+
+		Assert.Contains("SecondaryIndexCheckpointTracker", ex.Message);
+	}
+
+	[Fact]
+	public async Task Multiple_Dispose_Calls_Are_Safe() {
+		// Given
+		var disposeCalled = 0;
+		var tracker = new SecondaryIndexCheckpointTracker(10, 100, _ => Task.CompletedTask);
+
+		// When
+		tracker.Start(CancellationToken.None);
+
+		try {
+			await tracker.DisposeAsync();
+			disposeCalled++;
+
+			await tracker.DisposeAsync();
+			disposeCalled++;
+		} catch (Exception) {
+			// Exception would cause the test to fail
+		}
+
+		// Then
+		Assert.Equal(2, disposeCalled);
+	}
+
+	[Fact]
+	public async Task Concurrent_Dispose_Calls_Are_Safe() {
+		// Given
+		var tracker = new SecondaryIndexCheckpointTracker(10, 100, _ => Task.CompletedTask);
+		var exceptions = 0;
+
+		// When
+		tracker.Start(CancellationToken.None);
+
+		var disposeTasks = Enumerable.Range(0, 5)
+			.Select(_ => Task.Run(async () => {
+				try {
+					await tracker.DisposeAsync();
+				} catch (Exception) {
+					Interlocked.Increment(ref exceptions);
+				}
+			}))
+			.ToArray();
+
+		await Task.WhenAll(disposeTasks);
+
+		// Then
+		Assert.Equal(0, exceptions);
+	}
+
+	[Fact]
+	public async Task Disposal_During_Active_Commit_Allows_Commit_To_Complete() {
+		// Given
+		var commitStarted = new ManualResetEventSlim(false);
+		var commitBlocker = new ManualResetEventSlim(false);
+		var commitCompleted = false;
+
+		var tracker = new SecondaryIndexCheckpointTracker(5, 1000, _ => {
+			commitStarted.Set();
+			commitBlocker.Wait(100);
+			commitCompleted = true;
+			return Task.CompletedTask;
+		});
+
+		// When
+		tracker.Start(CancellationToken.None);
+
+		for (int i = 0; i < 5; i++) tracker.Increment();
+
+		Assert.True(commitStarted.Wait(100));
+
+		var disposeTask = Task.Run(async () => await tracker.DisposeAsync());
+
+		await Task.Delay(10);
+
+		commitBlocker.Set();
+
+		await disposeTask;
+
+		// Then
+		Assert.True(commitCompleted);
+	}
+}
