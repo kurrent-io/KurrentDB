e86221c295fb125b0b710a42b9c0679cee34a4eb
Timothy Coleman <timothy.coleman@gmail.com>
2025-05-09 14:42:37 +0100

    Increase log record max size to 32mb

--- src/EventStore.Core/TransactionLog/Chunks/TFConsts.cs
+++ src/EventStore.Core/TransactionLog/Chunks/TFConsts.cs
@@ -6,7 +6,7 @@ using System;
 namespace EventStore.Core.TransactionLog.Chunks;
 
 public static class TFConsts {
-	public const int MaxLogRecordSize = 16 * 1024 * 1024; // 16Mb ought to be enough for everything?.. ;)
+	public const int MaxLogRecordSize = 32 * 1024 * 1024; // 32Mb ought to be enough for everything?.. ;)
 	public const int LogRecordOverheadSize = 10000;
 	public const int EffectiveMaxLogRecordSize = MaxLogRecordSize - LogRecordOverheadSize;
 	public const int MidpointsDepth = 10;
