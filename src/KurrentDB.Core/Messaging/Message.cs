4798618bcee5c5857b87b43eb1f9d969765097cd
Roman Sakno <roman.sakno@kurrent.io>
2025-06-11 14:25:51 +0300

    Introduce synchronization groups instead of queue affinity

--- src/KurrentDB.Core/Messaging/Message.cs
+++ src/KurrentDB.Core/Messaging/Message.cs
@@ -24,6 +24,23 @@ public class DerivedMessageAttribute : Attribute {
 
 [BaseMessage]
 public abstract partial class Message(CancellationToken token = default) {
+	internal static readonly object UnknownAffinity = new();
+
 	[JsonIgnore]
 	public CancellationToken CancellationToken => token;
+
+	/// <summary>
+	/// Returns the synchronization affinity for the current message.
+	/// </summary>
+	/// <remarks>
+	/// The affinity changes the ordering guarantees for the two messages X and Y, if the published
+	/// in order, as follows:
+	/// 1. If <see langword="null"/> then there is no happens-before relationship between publish(X) and publish(Y);
+	/// 2. If X and Y have the same affinity (X.Affinity == Y.Affinity) then publish(X) happens before publish(Y);
+	/// 3. If X and Y have the different affinity (X.Affinity != Y.Affinity) then there is no happens-before
+	/// relationship between publish(X) and publish(Y), as for the case #1;
+	///
+	/// By default, the property returns the affinity object which behavior depends on the handler type.
+	/// </remarks>
+	public virtual object Affinity => UnknownAffinity;
 }
