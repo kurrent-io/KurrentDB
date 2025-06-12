786a0b990b9758d0137811b78a2847c3fb64ff7a
William Chong <william-chong@outlook.com>
2025-06-12 10:53:47 +0400

    Fixup

--- docker/test.sh
+++ docker/test.sh
@@ -15,14 +15,14 @@ for test in $tests; do
           --project "$test" \
           --report-trx \
           --results-directory "$output_directory/$proj"
-    else
-        # dotnet test \
-        #   --blame \
-        #   --blame-hang-timeout 5min \
-        #   --settings "$settings" \
-        #   --logger:"GitHubActions;report-warnings=false" \
-        #   --logger:trx \
-        #   --logger:"console;verbosity=normal" \
-        #   --results-directory "$output_directory/$proj" "$test/$proj.dll"
-    fi
+    # else
+    #     # dotnet test \
+    #     #   --blame \
+    #     #   --blame-hang-timeout 5min \
+    #     #   --settings "$settings" \
+    #     #   --logger:"GitHubActions;report-warnings=false" \
+    #     #   --logger:trx \
+    #     #   --logger:"console;verbosity=normal" \
+    #     #   --results-directory "$output_directory/$proj" "$test/$proj.dll"
+    # fi
 done
