f130e4d0e726e6261a75c6f55c2efd6ca362f484
William Chong <william-chong@outlook.com>
2025-06-12 12:43:52 +0400

    Show TRX

--- docker/test.sh
+++ docker/test.sh
@@ -15,14 +15,7 @@ for test in $tests; do
       --blame-hang-timeout 5min \
       --settings "$settings" \
       --logger:"GitHubActions;report-warnings=false" \
-      --logger:html \
       --logger:trx \
       --logger:"console;verbosity=normal" \
       --results-directory "$output_directory/$proj" "$test/$proj.dll"
-
-    html_files=$(find "$output_directory/$proj" -name "*.html")
-
-    for html in $html_files; do
-      cat "$html" > "$output_directory/test-results.html"
-    done
 done
