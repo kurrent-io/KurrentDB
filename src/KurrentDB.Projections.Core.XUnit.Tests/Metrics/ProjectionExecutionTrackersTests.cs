bf040d9ed381e9dd8659e34201a47c26e88975ea
Timothy Coleman <timothy.coleman@gmail.com>
2025-06-12 17:04:22 +0100

    fix accidental comments

--- src/KurrentDB.Projections.Core.XUnit.Tests/Metrics/ProjectionExecutionTrackersTests.cs
+++ src/KurrentDB.Projections.Core.XUnit.Tests/Metrics/ProjectionExecutionTrackersTests.cs
@@ -1,5 +1,5 @@
-//// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
-//// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
+// Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
+// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
 using System.Diagnostics.Metrics;
 using KurrentDB.Core.Metrics;
