4a0b946fa533524ed894e6107a6c77723345cac1
William Chong <william-chong@outlook.com>
2025-06-13 13:28:28 +0400

    Fixup

--- docker/test.sh
+++ docker/test.sh
@@ -14,18 +14,15 @@ for test in $tests; do
         dotnet exec \
           "$test/$proj.dll" \
           --report-trx \
-          --report-trx-filename schemaregistry.trx \
           --results-directory "$output_directory/$proj"
+    else
+        dotnet test \
+          --blame \
+          --blame-hang-timeout 5min \
+          --settings "$settings" \
+          --logger:"GitHubActions;report-warnings=false" \
+          --logger:trx \
+          --logger:"console;verbosity=normal" \
+          --results-directory "$output_directory/$proj" "$test/$proj.dll"
     fi
-
-    # else
-    #     dotnet test \
-    #       --blame \
-    #       --blame-hang-timeout 5min \
-    #       --settings "$settings" \
-    #       --logger:"GitHubActions;report-warnings=false" \
-    #       --logger:trx \
-    #       --logger:"console;verbosity=normal" \
-    #       --results-directory "$output_directory/$proj" "$test/$proj.dll"
-    # fi
 done
