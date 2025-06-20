4acb33300bd7cc3166121b76773332ec10e8284a
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-06-20 09:53:55 +0200

    Tiny namespace cleanup

--- src/KurrentDB.SecondaryIndexing/Indexes/Default/DefaultIndex.cs
+++ src/KurrentDB.SecondaryIndexing/Indexes/Default/DefaultIndex.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
 // Kurrent, Inc licenses this file to you under the Event Store License v2 (see LICENSE.md).
 
-using DotNext;
 using KurrentDB.Core.Services;
 
 namespace KurrentDB.SecondaryIndexing.Indexes.Default;
