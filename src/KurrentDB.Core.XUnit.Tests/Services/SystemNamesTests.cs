166057f6d7e641020416d5871b69ac3c5d9038f6
shaan1337 <sniper111@gmail.com>
2025-08-11 12:33:25 +0400

    suggestion

--- src/KurrentDB.Core.XUnit.Tests/Services/SystemNamesTests.cs
+++ src/KurrentDB.Core.XUnit.Tests/Services/SystemNamesTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
 // Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
+using System.Collections.Generic;
 using KurrentDB.Core.Services;
 using Xunit;
 
@@ -8,11 +9,11 @@ namespace KurrentDB.Core.XUnit.Tests.Services;
 
 public class SystemNamesTests {
 	[Fact]
-	public void IsMemoryStream_WithInMemoryStreamPrefix_ReturnsTrue()
+	public void IsInMemoryStream_WithInMemoryStreamPrefix_ReturnsTrue()
 	{
 		const string streamId = SystemStreams.InMemoryStreamPrefix + "custom";
-
 		Assert.True(SystemStreams.IsInMemoryStream(streamId));
+
 		Assert.False(SystemStreams.IsIndexStream(streamId));
 	}
 
@@ -20,32 +21,42 @@ public class SystemNamesTests {
 	public void IsIndexStream_WithIndexStreamPrefix_ReturnsTrue()
 	{
 		const string streamId = SystemStreams.IndexStreamPrefix + "custom";
-
 		Assert.True(SystemStreams.IsIndexStream(streamId));
+
 		Assert.False(SystemStreams.IsInMemoryStream(streamId));
 	}
 
 	[Fact]
-	public void IsVirtualStream_WithPredefinedVirtualStreams_ReturnsTrue()
+	public void IsInMemoryStream_WithPredefinedInMemoryStreams_ReturnsTrue()
 	{
 		Assert.True(SystemStreams.IsInMemoryStream(SystemStreams.NodeStateStream));
 		Assert.True(SystemStreams.IsInMemoryStream(SystemStreams.GossipStream));
 	}
 
+	public static IEnumerable<object[]> NonVirtualStreams()
+	{
+		yield return [SystemStreams.AllStream];
+		yield return [SystemStreams.EventTypesStream];
+		yield return [SystemStreams.StreamsStream];
+		yield return [SystemStreams.SettingsStream];
+		yield return ["category-stream"];
+		yield return ["regularstream"];
+		yield return ["idx-withoutdollar"];
+		yield return ["mem-withoutdollar"];
+		yield return [""];
+	}
 
 	[Theory]
-	[InlineData(SystemStreams.AllStream)]
-	[InlineData(SystemStreams.EventTypesStream)]
-	[InlineData(SystemStreams.StreamsStream)]
-	[InlineData(SystemStreams.SettingsStream)]
-	[InlineData("caregory-stream")]
-	[InlineData("regularstream")]
-	[InlineData("idx-withoutdollar")]
-	[InlineData("mem-withoutdollar")]
-	[InlineData("")]
-	public void IsVirtualStream_WithoutMemOrIdxPrefix_ReturnsFalse(string streamId)
+	[MemberData(nameof(NonVirtualStreams))]
+	public void IsInMemoryStream_WithNonVirtualStreams_ReturnsFalse(string streamId)
 	{
 		Assert.False(SystemStreams.IsInMemoryStream(streamId));
+	}
+
+	[Theory]
+	[MemberData(nameof(NonVirtualStreams))]
+	public void IsIndexStream_WithNonVirtualStreams_ReturnsFalse(string streamId)
+	{
 		Assert.False(SystemStreams.IsIndexStream(streamId));
 	}
 }
