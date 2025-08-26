703d508ce51733b9f7f15a4d6a419a88622a6299
Roman Sakno <roman.sakno@kurrent.io>
2025-08-26 09:44:11 +0300

    Fixed Qodana warnings

--- src/KurrentDB.Core.Tests/Index/IndexV1/table_index_with_corrupt_index_entries_should.cs
+++ src/KurrentDB.Core.Tests/Index/IndexV1/table_index_with_corrupt_index_entries_should.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using KurrentDB.Core.Exceptions;
 using KurrentDB.Core.Index;
 using KurrentDB.Core.Index.Hashes;
-using KurrentDB.Core.TransactionLog;
 using NUnit.Framework;
 
 namespace KurrentDB.Core.Tests.Index.IndexV1;
