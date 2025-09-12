2d7ed90b003f94e94c008244b1a93b5969f74747
Alexey Zimarev <alex@zimarev.com>
2025-09-12 16:12:46 +0200

    * Updated file headers * Added ad-hoc query check to prevent schema and data changes

--- src/KurrentDB.SecondaryIndexing/Storage/Models.cs
+++ src/KurrentDB.SecondaryIndexing/Storage/Models.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
-// Kurrent, Inc licenses this file to you under the Event Store License v2 (see LICENSE.md).
+// Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
 namespace KurrentDB.SecondaryIndexing.Storage;
 
