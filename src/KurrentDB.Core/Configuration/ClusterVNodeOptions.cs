86c325272db39a551686db18a1732846977f6207
Roman Sakno <roman.sakno@kurrent.io>
2025-11-06 07:52:52 +0200

    Fixed typo

--- src/KurrentDB.Core/Configuration/ClusterVNodeOptions.cs
+++ src/KurrentDB.Core/Configuration/ClusterVNodeOptions.cs
@@ -403,7 +403,7 @@ public partial record ClusterVNodeOptions {
 			"The ReaderThreadsCount parameter has been deprecated as of version 26.0.0 and currently has no effect. The workers automatically scale as necessary.")]
 		public int ReaderThreadsCount { get; init; } = 0;
 
-		[Description("The maximum number of read requests that can be processed concurrently. Set to '0' to no limit (Default)")]
+		[Description("The maximum number of read requests that can be processed concurrently. Set to '0' for unlimited (Default)")]
 		public long MaxConcurrentReadersCount { get; set; } = 0;
 
 		[Description("During large Index Merge operations, writes may be slowed down. Set this to the maximum " +
