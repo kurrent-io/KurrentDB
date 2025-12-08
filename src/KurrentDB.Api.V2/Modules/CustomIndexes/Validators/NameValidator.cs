6af4a58c7970596e9491e9dd8d444d233d93fa8b
shaan1337 <sniper111@gmail.com>
2025-12-08 11:27:01 +0400

    limit custom index name size

--- src/KurrentDB.Api.V2/Modules/CustomIndexes/Validators/NameValidator.cs
+++ src/KurrentDB.Api.V2/Modules/CustomIndexes/Validators/NameValidator.cs
@@ -9,6 +9,8 @@ namespace KurrentDB.Api.Modules.CustomIndexes.Validators;
 class NameValidator : ValidatorBase<NameValidator, string?> {
 	public NameValidator() =>
 		RuleFor(x => x)
+			.MinimumLength(1)
+			.MaximumLength(1000)
 			.Matches("^[a-z0-9_-]+$")
 			.WithMessage("Name can contain only lowercase alphanumeric characters, underscores and dashes")
 			.WithName("Name");
