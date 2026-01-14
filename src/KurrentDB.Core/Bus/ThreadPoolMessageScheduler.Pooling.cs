9182e8459c7175cfaa8b6cd0351b312d8678be6e
Roman Sakno <roman.sakno@kurrent.io>
2026-01-14 21:10:01 +0200

    Fixed race condition in the object pooling layer

--- src/KurrentDB.Core/Bus/ThreadPoolMessageScheduler.Pooling.cs
+++ src/KurrentDB.Core/Bus/ThreadPoolMessageScheduler.Pooling.cs
@@ -1,39 +1,32 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
 // Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
+using System.Collections.Concurrent;
 using System.Threading;
 
 namespace KurrentDB.Core.Bus;
 
 partial class ThreadPoolMessageScheduler {
-	private volatile AsyncStateMachine _firstNode;
+	private readonly ConcurrentQueue<AsyncStateMachine> _items = new();
+	private AsyncStateMachine _fastItem;
 
-	private void ReturnToPool(AsyncStateMachine node) {
-		AsyncStateMachine current;
-		do {
-			current = _firstNode;
-			node.NextInPool = current;
-		} while (Interlocked.CompareExchange(ref _firstNode, node, current) != current);
+	private void ReturnToPool(AsyncStateMachine item) {
+		if (_fastItem is not null || Interlocked.CompareExchange(ref _fastItem, item, null) is not null) {
+			_items.Enqueue(item);
+		}
 	}
 
 	private AsyncStateMachine RentFromPool() {
-		AsyncStateMachine current;
-		do {
-			current = _firstNode;
+		var result = _fastItem;
+		if (result is null || Interlocked.CompareExchange(ref _fastItem, null, result) != result) {
+			if (!_items.TryDequeue(out result))
+				result = new PoolingAsyncStateMachine(this);
+		}
 
-			if (current is null) {
-				current = new PoolingAsyncStateMachine(this);
-				break;
-			}
-		} while (Interlocked.CompareExchange(ref _firstNode, current.NextInPool, current) != current);
-
-		current.NextInPool = null;
-		return current;
+		return result;
 	}
 
 	private partial class AsyncStateMachine {
-		internal AsyncStateMachine NextInPool;
-
 		protected void ReturnToPool() => _scheduler.ReturnToPool(this);
 	}
 }
