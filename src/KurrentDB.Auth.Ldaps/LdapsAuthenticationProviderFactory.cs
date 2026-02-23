3b4cc1133f2fdffbd2aff34f61615c8d883d108f
Tim Coleman <timothy.coleman@gmail.com>
2026-02-23 09:16:06 +0000

    Timothycoleman/oauth cherry picks (#5517)
    Cherry picks back to 25.1 of recent OAuth improvements. 25.1 STS is out of support, but it is possible that we may want to release these in a 25.1 patch
    
    - https://github.com/kurrent-io/KurrentDB/pull/5506 [DB-1920] Fix OAuth tests
    - https://github.com/kurrent-io/KurrentDB/pull/5502 [DB-1923] Log useful information when interpreting auth plugin config files
    - https://github.com/kurrent-io/KurrentDB/pull/5511 [DB-1921] Add option to skip PKCE code_challenge_methods_supported validation
    https://github.com/kurrent-io/KurrentDB/pull/5514 [DB-1929] Allow OAuth and Ldaps plugins to consume configuration from standard sources
    
    
    * Get the IP address from the 'bridge' network (#5506)
    
    GHA no longer populates the IP address at the network settings root level
    
    GHA docker upgrade which took effect on 9th Feb 2026: https://github.blog/changelog/2026-01-30-docker-and-docker-compose-version-upgrades-on-hosted-runners/
    
    The particular docker change affecting us: https://docs.docker.com/engine/deprecated#top-level-network-properties-in-networksettings
    
    * Log useful information when interpreting auth plugin config files (#5502)
    
    If the server configuration specifies oauth or ldaps as the AuthenticationType, but doesn't include a corresponding configuration section, then the server fails to start with a confusing message
    
    ```
    [    1, 1,21:13:42.689,FTL] KurrentDB                      Host terminated unexpectedly.
    (Line: 1, Col: 1, Idx: 0) - (Line: 1, Col: 29, Idx: 28): Property 'AllowAnonymousEndpointAccess' not found on type 'KurrentDB.Auth.OAuth.OAuthAuthenticationPlugin+Settings'.
    ```
    
    This happens because when not finding the expected section, the ConfigParser attempts to interpret the whole file as that section.
    We've kept this behaviour for backwards compatibility, and added logging to make it clear.
    
    ```
    [62388, 2,09:26:53.569,INF] OAuthAuthenticationPlugin      Reading OAuth configuration from yaml file d:/EventStore/quick20/eventstore.yaml
    [62388, 2,09:26:53.570,INF] OAuthAuthenticationPlugin      Could not find section OAuth, interpreting whole file as OAuth section
    [62388, 2,09:26:53.602,FTL] KurrentDB                      Host terminated unexpectedly.
    (Line: 1, Col: 1, Idx: 0) - (Line: 1, Col: 3, Idx: 2): Property 'db' not found on type 'KurrentDB.Auth.OAuth.OAuthAuthenticationPlugin+Settings'.
    ```
    
    * Add option to skip PKCE code_challenge_methods_supported validation (#5511)
    
    * Add option to skip PKCE code_challenge_methods_supported validation
    
    The option is OAuth::DisableCodeChallengeMethodsSupportedValidation
    
    The default behaviour (false) is the current behaviour which validates code_challenge_methods_supported
    in line with rfc8414, and uses S256.
    
    Setting this option to true skips the validation, and uses S256 even if it is not listed as supported.
    
    This is necessary for Microsoft Entra, which does support S256 but does not populate code_challenge_methods_supported
    
    * refactor validation and add tests
    
    * doc
    
    * Allow OAuth and Ldaps plugins to consume configuration from standard sources (#5514)
    
    Before now, and in contrast to all the other configuration options, the OAuth and Ldaps plugins could only take their configuration from a file, and not from the environment or command line options.
    
    The change is backwards compatible:
    
    1. If KurrentDB:AuthenticationConfig is specified, then they still load their config from the specified file as before.
    2. Otherwise they load their config from the main IConfiguration object built by the server, which also matches the previous behaviour of loading the config from the main configuration file.
    
    We also have not modified the data passed to plugins discovered at runtime.

--- src/KurrentDB.Auth.Ldaps/LdapsAuthenticationProviderFactory.cs
+++ src/KurrentDB.Auth.Ldaps/LdapsAuthenticationProviderFactory.cs
@@ -1,24 +1,16 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
 // Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
-using EventStore.Plugins;
 using EventStore.Plugins.Authentication;
 
 namespace KurrentDB.Auth.Ldaps;
 
-internal class LdapsAuthenticationProviderFactory : IAuthenticationProviderFactory {
+internal class LdapsAuthenticationProviderFactory(LdapsSettings settings) : IAuthenticationProviderFactory {
 	public const int CachedPrincipalCount = 1000;
-	private readonly string _configPath;
-	private LdapsAuthenticationProvider _authenticationProvider;
-
-	public LdapsAuthenticationProviderFactory(string configPath) {
-		_configPath = configPath;
-	}
 
 	public IAuthenticationProvider Build(bool logFailedAuthenticationAttempts) {
-		var ldapsSettings = ConfigParser.ReadConfiguration<LdapsSettings>(_configPath, "LdapsAuth");
-		_authenticationProvider = new LdapsAuthenticationProvider(ldapsSettings,
+		var authenticationProvider = new LdapsAuthenticationProvider(settings,
 			CachedPrincipalCount, logFailedAuthenticationAttempts);
-		return _authenticationProvider;
+		return authenticationProvider;
 	}
 }
