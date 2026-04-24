26e7e6e4ed37d0688f8a9d1bd94c8f9fbfeefd15
Roman Sakno <roman.sakno@kurrent.io>
2026-04-24 11:38:41 +0300

    Fixed authorization

--- src/KurrentDB.SecondaryIndexing/FlightSql/FlightSqlServer.cs
+++ src/KurrentDB.SecondaryIndexing/FlightSql/FlightSqlServer.cs
@@ -1,15 +1,19 @@
 // Copyright (c) Kurrent, Inc and/or licensed to Kurrent, Inc under one or more agreements.
 // Kurrent, Inc licenses this file to you under the Kurrent License v1 (see LICENSE.md).
 
+using System.Security.Claims;
 using Apache.Arrow;
 using Apache.Arrow.Flight;
 using Apache.Arrow.Flight.Server;
 using Apache.Arrow.Flight.Sql;
 using Arrow.Flight.Protocol.Sql;
+using EventStore.Plugins.Authorization;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using KurrentDB.Core.Services;
 using KurrentDB.SecondaryIndexing.Query;
+using KurrentDB.Core.Services.Transport.Grpc;
 using FlightSqlServerHelpers = Apache.Arrow.Flight.Sql.FlightSqlServer;
 
 namespace KurrentDB.SecondaryIndexing.FlightSql;
@@ -28,8 +32,15 @@ namespace KurrentDB.SecondaryIndexing.FlightSql;
 /// DuckDB prepared statement.
 /// </remarks>
 /// <param name="engine">The query engine.</param>
-internal sealed partial class FlightSqlServer(IQueryEngine engine) : FlightServer {
-	public override Task<FlightInfo> GetFlightInfo(FlightDescriptor request, ServerCallContext context) {
+/// <param name="authProvider">Authorization provider.</param>
+internal sealed partial class FlightSqlServer(IQueryEngine engine, IAuthorizationProvider authProvider) : FlightServer {
+	private static readonly Operation ReadOperation = new Operation(Operations.Streams.Read)
+		.WithParameter(Operations.Streams.Parameters.StreamId(SystemStreams.AllStream));
+
+	public override async Task<FlightInfo> GetFlightInfo(FlightDescriptor request, ServerCallContext context) {
+		if (!await authProvider.CheckAccessAsync(context.User, ReadOperation, context.CancellationToken))
+			throw RpcExceptions.AccessDenied();
+
 		//A google.protobuf.Any message has a specific binary signature. It consists of two fields:
 		// Field 1 (type_url): Tag 0x0A (field number 1, wire type 2).
 		// Field 2 (value): Tag 0x12 (field number 2, wire type 2).
@@ -38,78 +49,86 @@ internal sealed partial class FlightSqlServer(IQueryEngine engine) : FlightServe
 
 		// process Arrow Flight command
 		if (request.Command.Span is [not discriminator, ..])
-			return PrepareQueryAsync(request.Command.Span, request, context.CancellationToken);
+			return await PrepareQueryAsync(request.Command.Span, request, context.CancellationToken);
 
-		var state = context.GetHttpContext().Features.Get<ConnectionState>();
-		if (state is null)
-			return Task.FromException<FlightInfo>(WrongServerState());
+		var state = context.GetHttpContext().Features.Get<ConnectionState>() ?? throw WrongServerState();
 
 		// process Arrow Flight SQL command
 		return FlightSqlServerHelpers.GetCommand(request) switch {
-			// TODO: Ad-hoc query execution doesn't return the schema for the dataset. It's not possible to check
-			// the potential problem with it since all major FlightSql clients use prepared statement execution
-			CommandStatementQuery query => PrepareQueryAsync(query, request, context.CancellationToken),
-			CommandPreparedStatementQuery query => GetPreparedStatementSchemaAsync(
-				query.PreparedStatementHandle,
-				state,
-				request,
+			CommandStatementQuery query =>
+				// TODO: Ad-hoc query execution doesn't return the schema for the dataset. It's not possible to check
+				// the potential problem with it since all major FlightSql clients use prepared statement execution
+				await PrepareQueryAsync(query, request, context.CancellationToken),
+			CommandPreparedStatementQuery query => await GetPreparedStatementSchemaAsync(query.PreparedStatementHandle, state, request,
 				context.CancellationToken),
-			_ => Task.FromException<FlightInfo>(FeatureNotSupported())
+			_ => throw FeatureNotSupported()
 		};
 	}
 
-	public override Task DoGet(FlightTicket ticket, FlightServerRecordBatchStreamWriter responseStream, ServerCallContext context) {
+	public override async Task DoGet(FlightTicket ticket, FlightServerRecordBatchStreamWriter responseStream, ServerCallContext context) {
+		if (!await authProvider.CheckAccessAsync(context.User, ReadOperation, context.CancellationToken))
+			throw RpcExceptions.AccessDenied();
+
 		var parsedTicket = Any.Parser.ParseFrom(ticket.Ticket);
 
-		// execute plain query
-		if (parsedTicket.Is(BytesValue.Descriptor))
-			return ExecuteQueryAsync(parsedTicket.Unpack<BytesValue>().Value, responseStream, context.CancellationToken);
-
-		var state = context.GetHttpContext().Features.Get<ConnectionState>();
-		if (state is null)
-			return Task.FromException<FlightInfo>(WrongServerState());
-
-		if (parsedTicket.Is(CommandPreparedStatementQuery.Descriptor))
-			return ExecutePreparedStatementAsync(
+		if (parsedTicket.Is(BytesValue.Descriptor)) {
+			// execute plain query
+			await ExecuteQueryAsync(parsedTicket.Unpack<BytesValue>().Value, responseStream, context.CancellationToken);
+		} else if (context.GetHttpContext().Features.Get<ConnectionState>() is not { } state) {
+			throw WrongServerState();
+		} else if (parsedTicket.Is(CommandPreparedStatementQuery.Descriptor)) {
+			// execute prepared statement
+			await ExecutePreparedStatementAsync(
 				parsedTicket.Unpack<CommandPreparedStatementQuery>().PreparedStatementHandle.Memory,
 				state,
 				responseStream, context.CancellationToken);
-
-		return Task.FromException<FlightInfo>(FeatureNotSupported());
+		} else {
+			throw FeatureNotSupported();
+		}
 	}
 
-	public override Task DoAction(FlightAction action, IAsyncStreamWriter<FlightResult> response, ServerCallContext context) {
-		var state = context.GetHttpContext().Features.Get<ConnectionState>();
-		if (state is null)
-			return Task.FromException(WrongServerState());
+	public override async Task DoAction(FlightAction action, IAsyncStreamWriter<FlightResult> response, ServerCallContext context) {
+		if (!await authProvider.CheckAccessAsync(context.User, ReadOperation, context.CancellationToken))
+			throw RpcExceptions.AccessDenied();
 
-		return action.Type switch {
-			// Create prepared statement
-			SqlAction.CreateRequest => CreatePreparedStatementAsync(
-				FlightSqlUtils.ParseAndUnpack<ActionCreatePreparedStatementRequest>(action.Body),
-				state,
-				response,
-				context.CancellationToken),
+		var state = context.GetHttpContext().Features.Get<ConnectionState>() ?? throw WrongServerState();
 
-			// Destroy prepared statement
-			SqlAction.CloseRequest => ClosePreparedStatementAsync(
-				FlightSqlUtils.ParseAndUnpack<ActionClosePreparedStatementRequest>(action.Body).PreparedStatementHandle.Memory,
-				state,
-				response,
-				context.CancellationToken),
-
-			// Python driver sends this undocumented action type, which is no-op in our case
-			"CloseSession" => response.WriteAsync(new FlightResult(ByteString.Empty)),
-			_ => Task.FromException(CreateException(StatusCode.Unimplemented,
-				$"Action {action.Type} is not supported"))
-		};
+		switch (action.Type) {
+			case SqlAction.CreateRequest:
+				// Create prepared statement
+				await CreatePreparedStatementAsync(
+					FlightSqlUtils.ParseAndUnpack<ActionCreatePreparedStatementRequest>(action.Body),
+					state,
+					response,
+					context.CancellationToken);
+				break;
+			case SqlAction.CloseRequest:
+				// Destroy prepared statement
+				await ClosePreparedStatementAsync(
+					FlightSqlUtils.ParseAndUnpack<ActionClosePreparedStatementRequest>(action.Body).PreparedStatementHandle.Memory,
+					state,
+					response,
+					context.CancellationToken);
+				break;
+			case "CloseSession":
+				// Python driver sends this undocumented action type, which is no-op in our case
+				await response.WriteAsync(new FlightResult(ByteString.Empty));
+				break;
+			default:
+				throw CreateException(StatusCode.Unimplemented,
+					$"Action {action.Type} is not supported");
+		}
 	}
 
 	public override async Task DoPut(FlightServerRecordBatchStreamReader requestStream,
 		IAsyncStreamWriter<FlightPutResult> responseStream,
-		ServerCallContext context)
-		=> await DoPut(FlightSqlServerHelpers.GetCommand(await requestStream.FlightDescriptor), requestStream,
+		ServerCallContext context) {
+		if (!await authProvider.CheckAccessAsync(context.User, ReadOperation, context.CancellationToken))
+			throw RpcExceptions.AccessDenied();
+
+		await DoPut(FlightSqlServerHelpers.GetCommand(await requestStream.FlightDescriptor), requestStream,
 			responseStream, context);
+	}
 
 	private Task DoPut(IMessage? message,
 		FlightServerRecordBatchStreamReader requestStream,
@@ -130,18 +149,19 @@ internal sealed partial class FlightSqlServer(IQueryEngine engine) : FlightServe
 		};
 	}
 
-	public override Task<Schema> GetSchema(FlightDescriptor request, ServerCallContext context) {
-		var state = context.GetHttpContext().Features.Get<ConnectionState>();
-		if (state is null)
-			return Task.FromException<Schema>(WrongServerState());
+	public override async Task<Schema> GetSchema(FlightDescriptor request, ServerCallContext context) {
+		if (!await authProvider.CheckAccessAsync(context.User, ReadOperation, context.CancellationToken))
+			throw RpcExceptions.AccessDenied();
+
+		var state = context.GetHttpContext().Features.Get<ConnectionState>() ?? throw WrongServerState();
 
 		return FlightSqlServerHelpers.GetCommand(request) switch {
-			CommandStatementQuery query => GetQuerySchemaAsync(query.Query.AsMemory(), context.CancellationToken),
-			CommandPreparedStatementQuery query => GetPreparedStatementSchemaAsync(
+			CommandStatementQuery query => await GetQuerySchemaAsync(query.Query.AsMemory(), context.CancellationToken),
+			CommandPreparedStatementQuery query => await GetPreparedStatementSchemaAsync(
 				query.PreparedStatementHandle.Memory,
 				state,
 				context.CancellationToken),
-			_ => Task.FromException<Schema>(FeatureNotSupported())
+			_ => throw FeatureNotSupported(),
 		};
 	}
 
@@ -153,4 +173,10 @@ internal sealed partial class FlightSqlServer(IQueryEngine engine) : FlightServe
 
 	private static RpcException FeatureNotSupported()
 		=> CreateException(StatusCode.Unimplemented, "The feature is not supported");
+}
+
+file static class ServerCallContextExtensions {
+	extension(ServerCallContext context) {
+		public ClaimsPrincipal User => context.GetHttpContext().User;
+	}
 }
