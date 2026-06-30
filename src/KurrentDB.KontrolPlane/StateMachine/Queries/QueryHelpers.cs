cdf6ad95c625b87168f564826e9b4b12cfc7ccfa
Roman Sakno <roman.sakno@kurrent.io>
2026-06-30 21:01:24 +0300

    Migration to extension block

--- src/KurrentDB.KontrolPlane/StateMachine/Queries/QueryHelpers.cs
+++ src/KurrentDB.KontrolPlane/StateMachine/Queries/QueryHelpers.cs
@@ -7,26 +7,22 @@ using Kurrent.Quack;
 namespace KurrentDB.KontrolPlane.StateMachine.Queries;
 
 internal static class QueryHelpers {
-	public static QueryResult<string, AllDatabasesQuery> GetDatabases(
-		this DuckDBAdvancedConnection connection)
-		=> connection.ExecuteQuery<string, AllDatabasesQuery>();
+	extension(DuckDBAdvancedConnection connection) {
+		public QueryResult<string, AllDatabasesQuery> GetDatabases()
+			=> connection.ExecuteQuery<string, AllDatabasesQuery>();
 
-	public static QueryResult<(string Id, ulong Epoch), AllDatabasesWithEpochQuery> GetDatabasesWithEpoch(
-		this DuckDBAdvancedConnection connection)
-		=> connection.ExecuteQuery<(string, ulong), AllDatabasesWithEpochQuery>();
+		public QueryResult<(string Id, ulong Epoch), AllDatabasesWithEpochQuery> GetDatabasesWithEpoch()
+			=> connection.ExecuteQuery<(string, ulong), AllDatabasesWithEpochQuery>();
 
-	public static QueryResult<ValueTuple<string>, (EndPoint Address, bool IsReadOnlyReplica, bool IsLeader), AllNodesQuery> GetDatabaseNodes(
-		this DuckDBAdvancedConnection connection,
-		string databaseId)
-		=> connection.ExecuteQuery<ValueTuple<string>, (EndPoint, bool, bool), AllNodesQuery>(new(databaseId));
+		public QueryResult<ValueTuple<string>, (EndPoint Address, bool IsReadOnlyReplica, bool IsLeader), AllNodesQuery> GetDatabaseNodes(
+			string databaseId)
+			=> connection.ExecuteQuery<ValueTuple<string>, (EndPoint, bool, bool), AllNodesQuery>(new(databaseId));
 
-	public static QueryResult<ValueTuple<string>, (EndPoint Address, ulong Epoch, bool IsReadOnlyReplica), LeaderQuery> GetDatabaseLeader(
-		this DuckDBAdvancedConnection connection,
-		string databaseId)
-		=> connection.ExecuteQuery<ValueTuple<string>, (EndPoint, ulong, bool), LeaderQuery>(new(databaseId));
+		public QueryResult<ValueTuple<string>, (EndPoint Address, ulong Epoch, bool IsReadOnlyReplica), LeaderQuery> GetDatabaseLeader(
+			string databaseId)
+			=> connection.ExecuteQuery<ValueTuple<string>, (EndPoint, ulong, bool), LeaderQuery>(new(databaseId));
 
-	public static QueryResult<ValueTuple<string>, (string Description, ulong Epoch), DatabaseQuery> GetDatabase(
-		this DuckDBAdvancedConnection connection,
-		string databaseId)
-		=> connection.ExecuteQuery<ValueTuple<string>, (string, ulong), DatabaseQuery>(new(databaseId));
+		public QueryResult<ValueTuple<string>, (string Description, ulong Epoch), DatabaseQuery> GetDatabase(string databaseId)
+			=> connection.ExecuteQuery<ValueTuple<string>, (string, ulong), DatabaseQuery>(new(databaseId));
+	}
 }
