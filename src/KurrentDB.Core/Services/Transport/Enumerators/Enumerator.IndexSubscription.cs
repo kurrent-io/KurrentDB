e7c15a6cd7c6091e6bcd9815bee95776db9a73ef
shaan1337 <sniper111@gmail.com>
2026-07-23 10:55:51 +0400

    don't log errors on invalid index queries

--- src/KurrentDB.Core/Services/Transport/Enumerators/Enumerator.IndexSubscription.cs
+++ src/KurrentDB.Core/Services/Transport/Enumerators/Enumerator.IndexSubscription.cs
@@ -96,6 +96,7 @@ partial class Enumerator {
 				if (ex is not (
 				    OperationCanceledException or
 				    ReadResponseException.InvalidPosition or
+				    ReadResponseException.InvalidIndexQuery or
 				    ReadResponseException.IndexNotFound)) {
 					Log.Error(ex, "Subscription {SubscriptionId} to {IndexName} experienced an error.", _subscriptionId, _indexName);
 				}
